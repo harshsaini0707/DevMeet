6278f39558f893f28129bbd742480144f223fdc5
harshsaini0707 <harshsaini0172@gmail.com>
2024-09-29 18:34:31 +0530

    writting-auth-middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app =  express();
+const {adminAuth , userAuth} = require("../middlewares/auth");
 
-app.use("/hello",(req,res)=>{
-    res.send("HELLO HELLO !!!!!!")
+app.use("/admin", adminAuth)
+
+app.get("/user/login",(req,res)=>{
+    res.send(("Loggeg In Successfullyy!!"))
 })
-app.use("/test",(req,res)=>{
-    res.send("Testing....!!!")
+
+app.get("/user",userAuth,(req,res)=>{
+    res.send("User Getted!!")
+})
+
+app.get("/admin/getAllData",(req,res)=>{
+    //logic
+    res.send("All Data Sent")
+})
+
+app.get("/admin/delete",(req,res)=>{
+res.send("User Deleted")
 })
 
 app.listen(7777,()=>console.log("Server is Successfully Started!!"))
