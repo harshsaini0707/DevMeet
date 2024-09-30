cf4835ee62c76e1f9fdcd8ea3cb8a8d739bc92a7
harshsaini0707 <harshsaini0172@gmail.com>
2024-09-30 18:23:06 +0530

    API-writing

--- src/app.js
+++ src/app.js
@@ -7,24 +7,68 @@ app.use(express.json());
 
 
 app.post("/signup",async (req,res)=>{
+   const body = req.body;
+    //Creating a instance of  User Model
+    const user = new User(body);
  try{
-    const body = req.body;
-    const user = await User.create({
-        firstName : body.firstName,
-        lastName : body.lastName,
-        email : body.email,
-        password: body.password,
-        age: body.age,
-        gender : body.gender,
-       
-    })
-    res.send(user);
+    await user.save();
+    return res.json(user);
  }
  catch(err){
-    console.log(err);
+    res.send(err);
     
  }
 })
+
+app.get("/user",async(req,res)=>{
+   const userEmail = req.body.email;
+  
+   try {
+
+   const feed =  await User.find({email : userEmail});
+
+   if(!feed.length){
+     res.status(404).json({status:"Error Email not found"})
+   }else  return res.json(feed);
+
+   } catch (err) {
+      res.send(err);
+   }
+   
+  })
+
+
+app.get("/feed",async(req,res)=>{
+ 
+ try {
+   const feed = await User.find({});
+    res.json({feed});
+ } catch (err) {
+   res.send(err);
+ }
+})
+app.delete("/user",async (req,res)=>{
+   const id = req.body._id;
+   try {
+   
+   const user = await User.findByIdAndDelete(id);
+   res.send({status :"Successfully User Deleted!!"})
+   } catch (error) {
+      res.send(error);
+   }
+   
+})
+
+app.patch("/edit",async (req,res)=>{
+   const id = req.body._id;
+   try {
+      await User.findByIdAndUpdate({_id:id }, {firstName : "Java"})
+      res.json({upadate: "Sucessfully updated!!"})
+   } catch (error) {
+      res.send(error)
+   }
+})
+
 
 connectDB().then(()=>{
     console.log("DB Connected Successfully!!");
